42b37a8b1a273e97f21ee5857e3ce0a3eae9fb09
funmiayinde <funmiayinde11@gmail.com>
2022-06-09 15:58:10 +0100

    update nextConfig file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  publicRuntimeConfig: {
+    PORT: process.env.PORT,
+    NEXT_PUBLIC_LANG: process.env.NEXT_PUBLIC_LANG,
+    NEXT_PUBLIC_NAME: process.env.NEXT_PUBLIC_NAME,
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+    NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  }
 }
 
 module.exports = nextConfig
