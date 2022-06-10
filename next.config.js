d8514f268daf11c1e93f32d9a9aef507f8dd4a63
funmiayinde <funmiayinde11@gmail.com>
2022-06-10 06:17:01 +0100

    ensuring .env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
 const nextConfig = {
   reactStrictMode: true,
   env: {
+    PORT: process.env.PORT,
+    NEXT_PUBLIC_LANG: process.env.NEXT_PUBLIC_LANG,
+    NEXT_PUBLIC_NAME: process.env.NEXT_PUBLIC_NAME,
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+    NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL:
+      process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  },
+  publicRuntimeConfig: {
     PORT: process.env.PORT,
     NEXT_PUBLIC_LANG: process.env.NEXT_PUBLIC_LANG,
     NEXT_PUBLIC_NAME: process.env.NEXT_PUBLIC_NAME,
